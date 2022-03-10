d62687afd549b083fe3fd67e1936bd3091d10f07
chishakitakashi <e7axu4shai@icloud.com>
2022-03-10 18:15:16 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+
+  devise_for :admins, skip: [:registrations], controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+  }
+
+  authenticated :user do
+    devise_scope :admin do
+      get 'admins/sign_up' => 'devise/registrations#new', as: :new_admin_registration
+      post 'admins/sign_up' => 'devise/registrations#create', as: :admin_registration
+      get 'admins/sign_out' => 'devise/registrations#destroy', as: :destroy_admin_registration
+    end
+  end
+
+  devise_for :users, skip: [:registrations], controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+  }
+
+  resources :users
+  root"posts#index"
+  resources :posts
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
